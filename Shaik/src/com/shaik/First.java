e9b14c9644edaa78f5a875e28e9a799a68e5002a
meeravalishaik5201 <meeravalishaik5201@outlook.com>
2025-05-25 17:44:08 +0530

    Third commit

--- Shaik/src/com/shaik/First.java
+++ Shaik/src/com/shaik/First.java
@@ -5,5 +5,9 @@ public class First {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Saida");
+		int a=200;
+		int b=300;
+		System.out.println(a+b);
+		System.out.println("a value"+a+"B value"+b);
 	}
 }
